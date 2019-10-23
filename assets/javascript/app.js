41dd294eb0ceeee9c0d1d240b86b0322c11e3d40
Herman Liu <hermanliu95@gmail.com>
2019-10-23 14:50:36 -0700

    created a button click event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,44 @@
 $(document).ready(function () {
-
+    createBtn();
 })
 
-//My Topic will be on food
-var topics =
-    [
-        'apple', 'carrot', 'watermelon', 'avocado', 'cucumber', 'asparagus', 'celery', 'broccoli',
-        'banana', 'corn', 'mushrooms', 'pinapple', 'pumpkin', 'cocounut', 'kiwi'
-    ]
 
 
-
-for (var i = 0; i < topics.length; i++) {
-    //console.log(topics[i]);
-    var buttons = topics[i]
-    $('.btn-container').append('<button>' + buttons + '</button>');
-    $('<button>' + buttons + '</button>').attr('id', 'btnId')
+//Create a function that loops through the array and create button.
+function createBtn() {
+    var topics =
+        [
+            'apple', 'carrot', 'watermelon', 'avocado', 'cucumber', 'asparagus', 'celery', 'broccoli',
+            'banana', 'corn', 'mushrooms', 'pinapple', 'pumpkin', 'cocounut', 'kiwi'
+        ]
+    for (var i = 0; i < topics.length; i++) {
+        var btn = $("<button>" + topics[i] + "</button>");
+        btn.addClass("jsonData");
+        btn.attr("data-name", topics[i]);
+        btn.attr("onclick", "displayGifs('" + topics[i] + "')");
+        btn.appendTo("#buttons");
+    }
 }
 
 
-$('button').on('click', function () {
-    var food = $(this).attr("btnId");
-
-
-    const queryUrl = 'https://api.giphy.com/v1/gifs/search?q=' + food +
-
-        '&api_key=OvLuQaaXhKQu4K1uM7YL7gXdvstfNCiw&q=cat&limit=10';
-
+function displayGifs(topic) {
+    var topic = $(this).attr('data-name');
+    const queryUrl = 'https://api.giphy.com/v1/gifs/search?q=' +
+        topic + '&api_key=OvLuQaaXhKQu4K1uM7YL7gXdvstfNCiw&limit=10&offset=0&rating=G&lang=en';
+    const api_key = '&api_key=OvLuQaaXhKQu4K1uM7YL7gXdvstfNCiw';
     $.ajax({
         method: 'GET',
         url: queryUrl,
-        datatype: JSON,
     }).then(function (response) {
-        var results = (response.data[0].images.fixed_height)
-        console.log(response.data[0].images.fixed_height);
-        $('.gif-container').prepend(results)
-
+        if (response.data.length > 1) {
+            for (var i = 1; i < 9; i++) {
+                var results = response.data;
+                var img = $('<img>');
+                var imgURL = results[i].images.fixed_height.url;
+                img.attr('src', imgURL);
+                $('#gif-container').append(img);
+            }
+        }
     })
-})
+}
 
